43924e43dcb704fe0f7fbba1f3b6ebb6f3dcd677
Erik Woodworth <ewoodworth@atlantaregional.org>
2020-10-22 10:20:19 -0400

    removed log in config controller

--- controllers/configController.js
+++ controllers/configController.js
@@ -5,10 +5,7 @@ module.exports = {
     console.log('config query: ',req.query);
     config.find(req.query)
       // .sort({ date: -1 })
-      .then(dbModel => {
-        console.log(dbModel);
-        return res.json(dbModel);
-      })
+      .then(dbModel =>  res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
 }
